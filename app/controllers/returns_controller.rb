cd961bf964a76a56d28868d9e7cdaaea4843d995
kennpat <kennedypr@gmail.com>
2015-02-04 15:05:09 -0500

    Now have the ability to determine the service level you need

--- app/controllers/returns_controller.rb
+++ app/controllers/returns_controller.rb
@@ -1,4 +1,30 @@
 class ReturnsController < ApplicationController
+
+  def show
+    @return = Return.find(params['id'])
+
+    if @return.filer_experience == 'Slim to none'
+      @score = -20
+    elsif @return.filer_experience == 'Some experience'
+      @score = -10
+    else
+      @score = 0
+    end
+
+    @return.questions.each do |question|
+      @score = @score + question.score_value
+    end
+
+    if @score <= 20
+      @service_determination = 'You can probably handle this yourself'
+    elsif @score <50
+      @service_determination = 'You probably need some help with your return'
+    else
+      @service_determination = 'You should probably see a CPA'
+    end
+
+  end
+
   def new
     @return = Return.new
   end
@@ -15,6 +41,23 @@ class ReturnsController < ApplicationController
 
   def edit
     @return = Return.find(params[:id])
+  end
+
+  def update
+    @return = Return.find(params[:id])
+    @return_questions = params['return']['question_ids']
+    if @return.update(return_params)
+      @return_questions.each do |return_question|
+        if return_question != ''
+          question = Question.find(return_question.to_i)
+          assigned_question = ReturnQuestion.new(question_id: question.id, return_id: @return.id)
+          assigned_question.save
+        end
+      end
+      redirect_to return_path(@return), notice: 'return updated successfully'
+    else
+      render :edit
+    end
   end
 
   private
