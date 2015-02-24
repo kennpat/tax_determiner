3a84397fe43a4f925f12e5cba19538f4998507ee
kennpat <kennedypr@gmail.com>
2015-02-24 16:17:30 -0500

    changind production environment to show actionmailer errors

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   default from: 'form@file.me'
   def contact_mailer(contact_form_detail)
     @contact_form_detail = contact_form_detail
-    mail to: 'file.me@dnstartups.com', subject: 'File.me contact form submission'
+    mail to: 'neil@dnstartups.com', subject: 'File.me contact form submission'
   end
 end
