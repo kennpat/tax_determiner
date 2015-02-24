36f75a554d5a033efe42113b22371bdd86092acc
kennpat <kennedypr@gmail.com>
2015-02-23 23:16:11 -0500

    Added more verbiage for the lowest level of service on the show page, removed a binding pry

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,6 @@ class UserMailer < ApplicationMailer
   default from: 'form@file.me'
   def contact_mailer(contact_form_detail)
     @contact_form_detail = contact_form_detail
-    binding.pry
     mail to: 'file.me@dnstartups.com', subject: 'File.me contact form submission'
   end
 end
