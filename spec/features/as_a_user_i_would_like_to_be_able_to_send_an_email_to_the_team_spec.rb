6647488d59f1ee3fcbbefc576979a94358fa65db
kennpat <kennedypr@gmail.com>
2015-02-25 20:19:41 -0500

    Added a fail condition test for the contact form

--- spec/features/as_a_user_i_would_like_to_be_able_to_send_an_email_to_the_team_spec.rb
+++ spec/features/as_a_user_i_would_like_to_be_able_to_send_an_email_to_the_team_spec.rb
@@ -15,8 +15,17 @@ feature 'as a user i want to be able to contact file.me', %{
     click_on 'Send Message'
     expect(ActionMailer::Base.deliveries.size).to eql(1)
     last_email = ActionMailer::Base.deliveries.last
-    expect(last_email).to have_content 'file.me@dnstartups.com'
+    expect(last_email).to have_content 'neil@dnstartups.com'
     expect(last_email).to have_content 'test@test.com'
+  end
+
+  scenario 'a user tries to submit a blank form and is presented with errors' do
+    visit new_contact_form_path
+    click_on 'Send Message'
+    expect(page).to have_content "Name can't be blank"
+    expect(page).to have_content "Email can't be blank"
+    expect(page).to have_content 'Email is invalid'
+    expect(page).to have_content "Message can't be blank"
   end
 
 end
