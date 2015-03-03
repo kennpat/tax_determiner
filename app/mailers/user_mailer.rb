0e066b1bef89f615d5399e00ec16fc997b64af58
kennpat <kennedypr@gmail.com>
2015-03-03 03:10:02 -0500

    changing email default

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,5 @@
 class UserMailer < ApplicationMailer
+  default from: 'form@file.me'
   def contact_mailer(contact_form_detail)
     @contact_form_detail = contact_form_detail
     mail to: 'neil@dnstartups.com', subject: 'File.me contact form submission'
