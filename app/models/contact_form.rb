7b1902af202f372ee0e299782a960362ff4fb79a
kennpat <kennedypr@gmail.com>
2015-03-05 02:16:09 -0500

    adding length validation for the contact form and set error messages to show in red

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -4,6 +4,7 @@ class ContactForm
   attr_accessor :name, :email, :message
 
   validates :name, :email, :message, presence: true
+  validates :message, length: { maximum: 1000 }
   validates_format_of :email, with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
 
 end
