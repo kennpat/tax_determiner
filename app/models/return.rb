45e11507fc9f6430b93f4d659c952e3cf3088b77
kennpat <kennedypr@gmail.com>
2015-03-05 02:31:23 -0500

    added missing validation for a state

--- app/models/return.rb
+++ app/models/return.rb
@@ -3,6 +3,7 @@ class Return < ActiveRecord::Base
   has_many :questions, through: :return_questions
 
   validates :return_type, presence: true
+  validates :filer_state, presence: true, on: :update
   validates :difficulty_score, presence: true, on: :update
   validates :filer_experience, presence: true, on: :update
   validates_format_of :filer_email, with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/, on: :update
